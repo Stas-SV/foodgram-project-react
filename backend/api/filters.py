150bf93a12cbca20c0f3f4e6990a4f5302ec2796
Станислав Савкин <stas.savkin@gmail.com>
2023-10-18 04:46:17 +0300

    Fix PEP8

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -18,13 +18,13 @@ class RecipesFilter(FilterSet):
         model = Recipe
         fields = ('tags', 'author', 'is_favorited', 'is_in_shopping_cart')
 
-    def is_favorited_filter(self, queryset, name, value):
+    def is_favorited_filter(self, queryset, value):
         user = self.request.user
         if value and user.is_authenticated:
             return queryset.filter(favorite_recipe__user=user)
         return queryset
 
-    def is_in_shopping_cart_filter(self, queryset, name, value):
+    def is_in_shopping_cart_filter(self, queryset, value):
         user = self.request.user
         if value and user.is_authenticated:
             return queryset.filter(shopping_recipe__user=user)
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import RecipeViewSet, TagViewsSet, IngredientViewSet, CustomUserViewSet
+from .views import (
+    RecipeViewSet,
+    TagViewsSet,
+    IngredientViewSet,
+    CustomUserViewSet
+)
 
 app_name = 'api'
 
